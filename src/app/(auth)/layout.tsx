af2077ab0691ff99e1d6075cac4c7c02e2440035
Harsh Sharma <harshbsharma1209@gmail.com>
2023-12-30 00:58:54 +0530

    Added Clerk

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+export default function AuthLayout({children}:{children: React.ReactNode}){
+    return (
+        
+            <div className="flex flex-row justify-center items-center h-full">
+            {children}
+            </div>
+        
+    )
+}
+    
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,11 +17,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+       <html lang="en">
       <body className={inter.className}>
       
             {children}
       </body>
     </html>
+    </ClerkProvider>
+   
   )
 }

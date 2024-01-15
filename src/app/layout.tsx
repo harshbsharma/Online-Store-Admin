01a1bd0d72c454e48e1c7c69720034293fdb723a
Harsh Sharma <harshbsharma1209@gmail.com>
2024-01-15 11:28:31 +0530

    Added SEO and OG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,52 @@ import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ['latin'] })
 
+
+const url = "https://cozycart.vercel.app/"
+const title=  'Cozy Cart Store Admin'
+const description = 'A Fully Featured Admin Dashboard with CMS and Inventroy Management'
+
 export const metadata: Metadata = {
-  title: 'Admin',
-  description: 'Admin for Online Store',
+  metadataBase: new URL(url),
+  title,
+  description,
+  keywords:[
+    "Cozy Cart",
+    "Cozy Cart Admin",
+    "Ecommerce Admin",
+    "Ecommerce CMS",
+    "Ecommerce Dashboard",
+    "Ecommerce Inventory Management",
+    "Ecommerce Admin Dashboard",
+  ],
+  creator: "Harsh Sharma",
+  // themeColor:[
+  //   { media: '(prefers-color-scheme: light)', color: 'white' },
+  //   { media: '(prefers-color-scheme: dark)', color: 'black' },
+  // ],
+  openGraph:{
+    type: 'website',
+    url,
+    title,
+    description,
+    siteName: title,
+    images: [
+      {
+        url: '/images/logo.png',
+      },
+    ],
+  },
+  twitter: {
+    title,
+    description,
+    creator: '@0xharsh_sharma',
+    images: '/src/assets/logo.png',
+   },
+  // icons: {  
+  //   icon: '/favicon.ico',
+  //   shortcut: '/favicon-16x16.png',
+  //   apple: '/apple-touch-icon.png',
+  // },
 }
 
 export default function RootLayout({
